aa86aa10608cf0b67f7b44d7d2a454f3494147a7
yoonemong <yoonemong@naver.com>
2021-01-21 06:10:17 +0900

    no message

--- springMvcTest/src/main/java/springmvctest/demo/SampleController.java
+++ springMvcTest/src/main/java/springmvctest/demo/SampleController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-@SessionAttributes("event")
+//@SessionAttributes("event")
 public class SampleController {
 
     @GetMapping("/events/form/name")
