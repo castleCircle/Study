577e58b551ef3fdd46c91651c0668432f64f5771
yoonemong <yoonemong@naver.com>
2021-01-21 23:08:59 +0900

    HttpEntity

--- springMvcTest/src/main/java/springmvctest/demo/EventApi.java
+++ springMvcTest/src/main/java/springmvctest/demo/EventApi.java
@@ -1,5 +1,7 @@
 package springmvctest.demo;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class EventApi {
 
     @PostMapping("/events")
-    public Event createEvent(@RequestBody Event event){
-        return event;
+    public Event createEvent(HttpEntity<Event> request){
+        MediaType contentType = request.getHeaders().getContentType();
+        System.out.println(contentType);
+        return request.getBody();
     }
 }
