4d3b4feec31b28a42288698935848591784ba10b
sungwon26 <yoonemong@naver.com>
2021-02-05 18:21:33 +0900

    enum method

---
+++ effectiveJava/src/item3/DirectionTest.java
@@ -0,0 +1,30 @@
+package item3;
+
+enum Direction{
+    EAST("east is good") {
+        public void getDirection(){
+            System.out.println("east");
+        }
+    },
+    WEST("west is good") {
+        public void getDirection(){
+            System.out.println("west");
+        }
+    };
+
+    private final String key;
+    public String getKey() {
+        return key;
+    }
+
+    private Direction(String str){
+        this.key = str;
+    }
+}
+
+public class DirectionTest {
+    public static void main(String[] args) {
+        System.out.println(Direction.EAST);
+        System.out.println(Direction.EAST.getKey());
+    }
+}
