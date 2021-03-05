49714beca9c5b83ca5bd98628949e983f7942787
윤성원 <yoonemong@plateer.com>
2021-03-05 12:56:59 +0900

    환경설정

--- springapplicationcontext/src/main/java/me/wony/springapplicationcontext/TestBookRepository.java
+++ springapplicationcontext/src/main/java/me/wony/springapplicationcontext/TestBookRepository.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
+/*
+프로파일
+ */
 @Repository
 @Profile("!test")
 public class TestBookRepository implements BookRepository{
