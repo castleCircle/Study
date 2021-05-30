3c8923daf7968faef95e3794b530a6987a6c5b5e
yoonemong <yoonemong@naver.com>
2021-05-31 08:29:27 +0900

    Neflix Zuul

---
+++ second-seervice/src/main/java/com/example/secondseervice/SecondSeerviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.secondseervice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSeerviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondSeerviceApplication.class, args);
+    }
+
+}
