ccbb162a1cc7b956878be79ce47f6a11a3562d07
윤성원 <yoonemong@naver.com>
2021-05-31 22:28:07 +0900

    response example

--- servlet/servlet/src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
+++ servlet/servlet/src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -14,38 +14,46 @@ public class ResponseHeaderServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        resp.setStatus(HttpServletResponse.SC_OK);
+
         resp.setStatus(HttpServletResponse.SC_OK);
 
+//        resp.setHeader("myHeader","test");
 //        resp.setHeader("Content-Type","text/plain;charset=UTF-8");
-        resp.setHeader("Cache-Control","no-cache, no-store, must-revalidate");
-        resp.setHeader("Pragma","no-cache");
-        resp.setHeader("my-header","hello");
-
-        content(resp);
-        cookie(resp);
-
+//        resp.setHeader("Cache-Control","no-cache,no-store");
+//        resp.setHeader("Content-Type","text/plain;charset=UTF-8");
+//        resp.setHeader("Cache-Control","no-cache, no-store, must-revalidate");
+//        resp.setHeader("Pragma","no-cache");
+//        resp.setHeader("my-header","hello");
+//
+//        content(resp);
+//        cookie(resp);
+//
         redirect(resp);
 
         PrintWriter writer = resp.getWriter();
         writer.println("안녕");
-
     }
 
     private void redirect(HttpServletResponse resp) throws IOException{
 //        resp.setStatus(HttpServletResponse.SC_FOUND);
 //        resp.setHeader("Location","/basic/hello-form.html");
-        resp.sendRedirect("/basic/hello-form.html");
+//        resp.sendRedirect("/basic/hello-form.html");
 
+        resp.setStatus(HttpServletResponse.SC_FOUND);
+        resp.setHeader("Location","/basic/hello-form.html");
     }
 
     private void content(HttpServletResponse response){
         response.setContentType("text/plain");
         response.setCharacterEncoding("utf-8");
+//        response.setContentType("text/plain");
+//        response.setCharacterEncoding("utf-8");
     }
 
     private void cookie(HttpServletResponse response){
-        Cookie cookie = new Cookie("myCookie", "good");
-        cookie.setMaxAge(600);
+        Cookie cookie = new Cookie("myCookie", "good1");
+        cookie.setMaxAge(6000);
         response.addCookie(cookie);
     }
 }
