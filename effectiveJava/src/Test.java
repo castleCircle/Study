3f02d1d9d1a0d4f566a14801befeb82d3554de9d
yoonemong@naver.com <sungwon26@plateer.com>
2021-07-07 15:53:50 +0900

    mvc

---
+++ effectiveJava/src/Test.java
@@ -0,0 +1,21 @@
+import java.util.Calendar;
+
+public class Test {
+
+    public static void main(String[] args) {
+        String a = "1";
+        String b = a;
+        changeString(b);
+        System.out.println(b);
+
+
+    }
+
+    public static void changeString(String b){
+        b = "a";
+    }
+
+
+
+
+}
